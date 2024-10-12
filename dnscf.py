8265dfe3bbb2728e1208e69c2f69be1d43d73d40
iambonboncat <phangsie@gmail.com>
2024-10-12 21:40:20 +0800

    Update dnscf.py

--- dnscf.py
+++ dnscf.py
@@ -89,13 +89,16 @@ def main():
     ip_addresses = ip_addresses_str.split(',')
     dns_records = get_dns_records(CF_DNS_NAME)
     push_plus_content = []
-    # 遍历 IP 地址列表
-    for index, ip_address in enumerate(ip_addresses):
-        # 执行 DNS 变更
-        dns = update_dns_record(dns_records[index], CF_DNS_NAME, ip_address)
-        push_plus_content.append(dns)
+    # 执行 DNS 最优IP变更
+    dns = update_dns_record(dns_records[0], CF_DNS_NAME, ip_addresses[0])
 
-    push_plus('\n'.join(push_plus_content))
+    # 遍历 IP 地址列表
+    # for index, ip_address in enumerate(ip_addresses):
+    #     # 执行 DNS 变更
+    #     dns = update_dns_record(dns_records[0], CF_DNS_NAME, ip_address)
+    #     push_plus_content.append(dns)
+
+    # push_plus('\n'.join(push_plus_content))
 
 if __name__ == '__main__':
     main()
